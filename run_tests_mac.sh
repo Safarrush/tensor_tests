9a4396b7d6b96020f0dbd546d35aad4f60dd6044
safar <Safarik288@gmail.com>
2023-11-06 00:13:02 +0500

    Вщту

--- run_tests_mac.sh
+++ run_tests_mac.sh
@@ -6,6 +6,12 @@ pip3 install -r requirements.txt
 
 pytest tests/*.py --alluredir=./allure-results
 
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+
+nvm install 14
+
+nvm use 14
+
 npm install --save-dev allure-commandline
 
 npx allure-commandline serve
